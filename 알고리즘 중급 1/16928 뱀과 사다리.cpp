ac471e640873d66ed676a82ed4bd4fa4ca2fc055
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-07-19 17:25:21 +0900

    16928 뱀과 사다리
    bfs 와 방문표시를 통해 수정완료

--- 알고리즘 중급 1/16928 뱀과 사다리.cpp
+++ 알고리즘 중급 1/16928 뱀과 사다리.cpp
@@ -1,92 +1,52 @@
 #include <iostream> 
+#include <queue>
 using namespace std;
 
 int u, v;
-
+bool check[100];
 int board[105];
-
-int n_min = -1;
-
-void go(int cnt, int next)
-
-{
-
-	if (next == 100)
-
-	{
-
-		if (n_min == -1 || n_min > cnt)
-
-			n_min = cnt;
-
-		return;
-
-	}
-
-	if (next > 100)
-
-		return;
-
-	if (cnt > 25)
-
-		return;
-
-
-
-
-
-	for (int i = 1; i <= 6; i++)
-
-	{
-
-		if (board[next + i])
-
-			go(cnt + 1, board[next + i]);
-
-		else
-
-			go(cnt + 1, next + i);
-
-	}
-
-}
-
-
-
 
 
 int main()
-
 {
-
 	cin >> u >> v;
 
 	int t1, t2;
 
+	queue<pair<int, int>> q;
+
 	for (int i = 0; i < u; i++)
-
 	{
-
 		cin >> t1 >> t2;
-
 		board[t1] = t2;
-
 	}
-
 	for (int i = 0; i < v; i++)
-
 	{
-
 		cin >> t1 >> t2;
-
 		board[t1] = t2;
-
 	}
 
-	go(0, 1);
+	q.push(make_pair(1, 0));
+	while (!q.empty())
+	{
+		auto temp = q.front();
+		check[temp.first] = true;
+		q.pop();
 
-	cout << n_min;
-
-
-
+		for (int i = 1; i <= 6; i++) {
+			if ((temp.first + i) == 100) {
+				cout << temp.second + 1;
+				return 0;
+			}
+			if ((temp.first + i) > 100)
+				continue;
+			if (board[temp.first + i] != 0 && check[board[temp.first + i]] == false) {
+				q.push(make_pair(board[temp.first + i], temp.second + 1));
+			}
+			else if(board[temp.first + i] == 0 && check[temp.first + i] == false  )
+			{
+				q.push(make_pair(temp.first + i, temp.second + 1));
+			}
+		}
+	}
 }
