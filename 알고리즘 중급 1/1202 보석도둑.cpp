578d9833102d595371616db7843e5e6c93203e79
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-07-27 14:34:30 +0900

    1202 보석도둑
    주석추가

--- 알고리즘 중급 1/1202 보석도둑.cpp
+++ 알고리즘 중급 1/1202 보석도둑.cpp
@@ -2,37 +2,50 @@
 #include <algorithm>
 #include <vector>
 #include <cstdio>
+#include <iostream>
+#include <set>
 using namespace std;
+
 struct jewel {
-	int m, v, w;
+	long long m;
+	long long v;
 };
-int main() {
+
+int main()
+{
 	int n, k;
-	scanf("%d %d", &n, &k);
-	vector<jewel> a(n + k);
-	for (int i = 0; i < n; i++) {
-		scanf("%d %d", &a[i].m, &a[i].v);
+	cin >> n >> k;
+	vector<jewel> jw(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> jw[i].m >> jw[i].v;
 	}
-	for (int i = 0; i < k; i++) {
-		scanf("%d", &a[i + n].m);
-		a[i + n].w = 1;
-	}
-	sort(a.begin(), a.end(), [](jewel u, jewel v) {
-		return u.m < v.m || (u.m == v.m && u.w < v.w);
+	sort(jw.begin(), jw.end(), [](jewel a, jewel b) {
+		return a.v > b.v;
 		});
-	priority_queue<int> q;
+	multiset<int> ml;
+	long long temp;
+	for (int i = 0; i < k; i++)
+	{
+		cin >> temp;
+		ml.insert(temp);
+	}
+
 	long long ans = 0;
-	for (auto& p : a) {
-		if (p.w == 0) {
-			q.push(p.v);
-		}
-		else {
-			if (!q.empty()) {
-				ans += (long long)q.top();
-				q.pop();
-			}
+	for (int i = 0; i < n; i++)
+	{
+		// 해당 인덱스의 무게와 가장 비슷한 무게를 찾는다. (이진 탐색)
+		auto it = ml.lower_bound(jw[i].m);
+
+		// it => ml.end()는 보석의 무게를 담을 수 있는 가방을 찾지 못한경우
+		if (it != ml.end())
+		{
+			// 적당한 인덱스를 찾은 경우 해당 보석은 담을 수 있으므로 총합에 더한다.
+			ans += jw[i].v;
+			ml.erase(it);
 		}
 	}
-	printf("%lld\n", ans);
-	return 0;
+
+	printf("%lld", ans);
+
 }
