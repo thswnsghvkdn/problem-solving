da1ebfb6dd90007b61dd2ca8ac4c63af778fcb0c
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-07-29 19:59:02 +0900

    12738 가장 긴 증가하는 부분순열

--- 알고리즘 중급 1/12738 가장 긴 증가하는 부분수열.cpp
+++ 알고리즘 중급 1/12738 가장 긴 증가하는 부분수열.cpp
@@ -5,14 +5,21 @@ using namespace std;
 
 int main()
 {
-	int n;
+	int n, temp;
 	cin >> n;
-	vector<int> v(n);
+	vector<int> v;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> v[i];
+		cin >> temp;
+		// lower_bound() (크거나 같은 원소중 가장 작은 원소의 위치)를 호출하여 temp가 들어갈 위치를 정한다.
+		auto it = lower_bound(v.begin(), v.end(), temp);
+		if (it == v.end()) // temp가 가장 큰 원소라면 마지막에 삽입
+			v.push_back(temp);
+		else
+		{
+			*it = temp; // temp보다 크거나 같은 원소를 temp로 대체한다.
+		}
 	}
-	sort(v.begin(), v.end());
-	v.erase(unique(v.begin(), v.end()), v.end());
 	cout << v.size();
+
 }
