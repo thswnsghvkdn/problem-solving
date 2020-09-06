9b21534176697364d800ef4a6bc06d487c51922d
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-09-06 18:40:10 +0900

    카카오 2018 추석 트래픽
    트래픽이 끝나는 기점으로 1초동안 나머지 트래픽의 개수를 카운트

--- 모의고사/2018 카카오 블라인드 추석 트래픽.cpp
+++ 모의고사/2018 카카오 블라인드 추석 트래픽.cpp
@@ -6,9 +6,11 @@
 #include <cmath>
 using namespace std;
 
+
+
 float t_min = 86400;
 float t_max = 0;
-pair<float , float> returnTime(string s) // 시작시간과 끝난 시간 반환
+pair<float, float> returnTime(string s) // 시작시간과 끝난 시간 반환
 {
 	stringstream ss;
 	ss.str(s);
@@ -17,8 +19,8 @@ pair<float , float> returnTime(string s) // 시작시간과 끝난 시간 반환
 	ss >> temp; // 시작시간을 가져온다. 
 	float dur;
 	ss >> dur; // 처리시간을 가져온다.
-	float hour = ((temp[0] - '0') * 10 + ( temp[1] - '0') ) * 3600; // 시간을 초로
-	float min = ((temp[3] - '0') * 10 + (temp[4] - '0') ) * 60; // 분을 초로
+	float hour = ((temp[0] - '0') * 10 + (temp[1] - '0')) * 3600; // 시간을 초로
+	float min = ((temp[3] - '0') * 10 + (temp[4] - '0')) * 60; // 분을 초로
 	temp = temp.substr(6); // 나머지 초를 가져온다.
 	float sec = stof(temp);
 	float end = hour + min + sec;
@@ -28,14 +30,17 @@ pair<float , float> returnTime(string s) // 시작시간과 끝난 시간 반환
 		t_min = start;
 	if (end > t_max)
 		t_max = end;
-	return make_pair(start , end);
+	return make_pair(start, end);
 }
 bool inTime(pair<float, float> v, float time)
 {
-	if (time <= v.first && v.first <= (time + 1))
-		return true;
-	if (time <= v.second && v.second <= (time + 1))
-		return true;
+	float start = time;
+	float end = time + 0.999;
+	bool one = false, two = false;
+
+	if (v.first < end || abs(v.first - end) < 0.0008)
+		if(v.second > start || abs(v.second - start) < 0.0008)
+		     return true;
 	return false;
 }
 int solution(vector<string> lines)
@@ -47,27 +52,27 @@ int solution(vector<string> lines)
 	{
 		v[i] = returnTime(lines[i]);
 	}
-	for (float time = t_min; time <= t_max; time= time+0.001)
+	for (int i = 0; i < lines.size(); i++) // 하나의 트래픽이 끝나는 시간을 기준으로 1초동안 나머지 트래픽의 개수를 세었다.ㄴ
 	{
+		float time = v[i].second;
 		int tot = 0;
-		for (int i = 0; i < lines.size(); i++)
+		for (int j = 0; j < lines.size(); j++)
 		{
-			if (inTime(v[i], time)) tot++;
+			if (inTime(v[j], time)) tot++;
 		}
 		if (answer < tot)
 			answer = tot;
 	}
-	char* str = (char*)"computer";
-	
-	cout << str[4] ;
+
 	return answer;
 
 }
 int main()
 {
-	solution({
-"2016-09-15 01:00:04.002 2.0s",
-"2016-09-15 01:00:07.000 2s",
+	solution({ 
+"2016-09-15 01:00:04.001 2.0s",
+"2016-09-15 01:00:07.000 2s"
+
 /*"2016-09-15 20:59:58.299 0.8s",
 "2016-09-15 20:59:58.688 1.041s",
 "2016-09-15 20:59:59.591 1.412s",
@@ -75,6 +80,6 @@ int main()
 "2016-09-15 21:00:00.741 1.581s",
 "2016-09-15 21:00:00.748 2.31s",
 "2016-09-15 21:00:00.966 0.381s",
-"2016-09-15 21:00:02.066 2.62s"*/
-		});
+"2016-09-15 21:00:02.066 2.62s"
+		*/});
 }
