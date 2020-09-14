fd0838b9ad8aa78fa67a3c8698f127400fe5075d
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-09-14 22:48:45 +0900

    14425 문자열 집합
    Trie

--- 알고리즘 중급 1/14425 문자열 집합.cpp
+++ 알고리즘 중급 1/14425 문자열 집합.cpp
@@ -1,27 +1,64 @@
 #include <iostream>
 #include <vector>
-#include <string>
+
 using namespace std;
 
-int main()
-{
-	int  n, m;
-	cin >> n >> m;
-	string temp;
-	vector<string> v1, v2;
-	for (int i = 0; i < n + m; i++)
-	{
-		cin >> temp;
-		if (i >= n) v2.push_back(temp);
-		else v1.push_back(temp);
+struct Node {
+	bool isEnd;
+	vector<Node*> node;
+	Node() {
+		node.assign(26, nullptr);
+		isEnd = false;
 	}
-	int cnt = 0;
+};
+void insertNode(Node* node, string str, int index)
+{
+	if (index == str.size()) {
+		node->isEnd = true;
+		return;
+	}
+	if(node->node[str[index] - 'a'] == nullptr)
+	{
+		node->node[str[index] - 'a'] = new Node();
+	}
+	insertNode(node->node[str[index] - 'a'], str, index + 1);
+}
+bool find(Node* node, string str)
+{
+	if (str == "")
+	{
+		if (node->isEnd) return true;
+		else return false;
+	}
+	else if (node->node[str[0] - 'a'] == NULL)
+		return false;
+
+	return find(node->node[str[0] - 'a'], str.substr(1));
+}
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<string> s1(n);
+	vector<string> s2(m);
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < m; j++)
-		{
-			if (v1[i] == v2[j]) cnt++;
-		}
+		string temp;
+		cin >> temp;
+		s1[i] = temp;
 	}
+	for (int i = 0; i < m; i++)
+	{
+		string temp;
+		cin >> temp;
+		s2[i] = temp;
+	}
+	Node node;
+	for (int i = 0; i < n; i++)
+		insertNode(&node, s1[i], 0);
+	int cnt = 0;
+	for (int i = 0; i < m; i++)
+		if (find(&node, s2[i])) cnt++;
+
 	cout << cnt;
+	return 0;
 }
