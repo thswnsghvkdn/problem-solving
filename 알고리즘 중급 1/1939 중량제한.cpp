841e63fc681ed350776daa6761043034ecdd6a49
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-10-01 22:51:30 +0900

    1939 중량제한
    다리를 건널 수 있는 중량을 0 ~ MAX 를 이분탐색으로 찾는다
    이분탐색에서 mid값을 bfs로 순회하며 해당 무게로 다리를 건널 수 있는지 검사한다.

--- 알고리즘 중급 1/1939 중량제한.cpp
+++ 알고리즘 중급 1/1939 중량제한.cpp
@@ -2,7 +2,7 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
-#define MAX 100000
+#define MAX 100010
 
 using namespace std;
 
@@ -16,7 +16,7 @@ struct map {
 bool find_weight(vector<vector<map>>& v, int w)
 {
 	queue<int> q;
-	vector<bool> visit(100000); // 방문 check
+	vector<bool> visit(10010); // 방문 check
 	visit[land1] = true;
 	q.push(land1);
 
@@ -25,42 +25,38 @@ bool find_weight(vector<vector<map>>& v, int w)
 		auto temp = q.front();
 		if (temp == land2) return true; // bfs도중 목적지에 도착한 경우 종료
 		q.pop();
-		
+
 		for (int i = 0; i < v[temp].size(); i++) // 인수로 들어온 무게보다 큰 가중치만 큐에 삽입
-			if (v[temp][i].weight >= w && !visit[v[temp][i].opposite]) {
+			if (v[temp][i].weight >= w && !visit[v[temp][i].opposite])
+			{
 				visit[v[temp][i].opposite] = true;
 				q.push(v[temp][i].opposite);
 			}
 	}
-	return false; 
+	return false;
 }
 
 
 int main()
 {
 	int col, row;
-	std::cin >> row >> col;
-	
-	vector<vector<map>> v(col + 1);
+	cin >> row >> col;
+
+	vector<vector<map>> v(col + 10);
 
 	int c, r, w;
 	int max_dis = 0;
-	for(int i = 0 ; i < col ; i++)
+	for (int i = 0; i < col; i++)
 	{
-		std::cin >> c >> r >> w;
+		cin >> c >> r >> w;
 		v[c].push_back({ r, w });
 		v[r].push_back({ c, w });
 		if (max_dis < w)
 			max_dis = w;
 	}
-	std::cin >> land1 >> land2;
-	int min_dis;
+	cin >> land1 >> land2;
+	int min_dis = 0;
 
-
-	for (int i = 0; i < v[land1].size(); i++) // 두 섬간 가중치를 최소 값으로 지정
-		if (v[land1][i].opposite == land2)
-			min_dis = v[land1][i].weight;
-	
 	int ans = min_dis;
 	while (min_dis <= max_dis)
 	{
