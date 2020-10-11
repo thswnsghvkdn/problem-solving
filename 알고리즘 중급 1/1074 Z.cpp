946359e3e74cb858fd01b671ffbe3c0a67913fcf
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-10-11 14:45:32 +0900

    1074 Z 재시도

--- 알고리즘 중급 1/1074 Z.cpp
+++ 알고리즘 중급 1/1074 Z.cpp
@@ -3,35 +3,38 @@
 #include <math.h>
 using namespace std;
 int ans;
-int row, col;
-void find_key(int n, int r, int c)
+int r, c;
+void find_key(int n, int row, int col)
 {
 	if (r == row && c == col) {
-		cout << ans; // 1씩 z 방향으로 증가한 결과를 출력한다.
+		cout << ans; // z 방향으로 증가한 결과를 출력한다.
 		return;
 	}
-	if (!n) // 2의 0제곱이 될 경우에 1씩 더한다.
-	{
-		ans++;
-		return;
-	}
-	int stand = pow(2, n - 1);
-	find_key(n - 1, r, c);
 
-	find_key(n - 1, r, c + stand);
+	int stand = pow(2, n - 1); // 행과 열이 증가할 기준 지수의 반만큼 증가시킬 것
 
-	find_key(n - 1, r + stand, c);
+	// 1구역 조건식
+	if (row <= r && r < (row + stand) && col <= c && c < (col + stand)) 
+		find_key(n - 1, row, col);
+	ans += ( pow(2, n - 1) * pow(2, n - 1) ); // 1구역이 아닐 경우 1구역의 합을 정답에 더한다. 
 
-	find_key(n - 1, r + stand, c + stand);
+	if (row <= r && r < (row + stand) && (col + stand) <= c  && c < (col + 2 * stand))
+		find_key(n - 1, row, col + stand);
+	ans += (pow(2, n - 1) * pow(2, n - 1)); // 2구역이 아닐 경우 2구역의 합을 정답에 더한다. 
 
+	if ( (row + stand) <= r && r < (row + 2 * stand) && col <= c && c < (col + stand))
+		find_key(n - 1, row + stand, col);
 
+	ans += (pow(2, n - 1) * pow(2, n - 1)); // 3구역이 아닐 경우 3구역의 합을 정답에 더한다. 
+	if ((row + stand) <= r && r < (row + 2 * stand) && (col + stand) <= c && c < (col + 2 * stand))
+		find_key(n - 1, row + stand, col + stand);
 
 }
 
 int main()
 {
 	int n;
-	cin >> n >> row >> col;
+	cin >> n >> r >> c;
 	find_key(n, 0, 0);
 
 	return 0;
