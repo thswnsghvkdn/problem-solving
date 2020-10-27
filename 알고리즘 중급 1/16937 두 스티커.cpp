86c80dc54fb2778c1d78c0e0abe0908d1177fae4
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-10-27 15:00:52 +0900

    16937 두 스티커
    두 번째 스티커판의 넓이를 계산할때 r1 + r2 그리고 두개의 컬럼중 큰 값을 높이에서 빼줘야 하는 것을 놓쳐서 계속 틀렸다.

--- 알고리즘 중급 1/16937 두 스티커.cpp
+++ 알고리즘 중급 1/16937 두 스티커.cpp
@@ -7,53 +7,37 @@ struct Point  // 사각형의 높이 넓이
 {
 	int r, c;
 };
+int num; // 개수 
+int height, width; // 스티커판의 높이 넓이 
 
-int sticker(int r, int c, int r_input, int c_input) // 스티커를 붙일 수 있는지 검사 
+bool checkSticker(int r1, int c1, int r2, int c2)
 {
-	if (r >= r_input && c >= c_input) return 1; // 스티커를 돌리지 않고 붙였으면 1
-	if (r >= c_input && c >= r_input) return 2; // 스티커를 돌려서 붙여야 하면 2
-	else return 0;
+	if (width >= (r1 + r2) && height >= max(c1, c2)) return true;
+	if (width >= max(r1 , r2) && height >= (c1 + c2)) return true;
+	return false;
 }
+
+bool isPossible(int r1, int c1, int r2, int c2)
+{
+	if (checkSticker(r1, c1, r2, c2)) return true;
+	if (checkSticker(c1, r1, r2, c2)) return true;
+	if (checkSticker(r1, c1, c2, r2)) return true;
+	if (checkSticker(c1, r1, c2, r2)) return true;
+	return false;
+}
+
 int main()
 {
-	int row, col;
-	cin >> row >> col;
-	int num;
+	cin >> width >> height;
 	cin >> num;
 	vector<Point> v(num);
-	int nr, nc;
 	for (int i = 0; i < num; i++)
-	{
 		cin >> v[i].r >> v[i].c;
-	}
+	
+	int ans = 0;
+	for(int i  =0 ; i < num ; i++)
+		for (int j = i + 1; j < num; j++)
+			if (isPossible(v[i].r, v[i].c, v[j].r, v[j].c)) ans = max(ans, ((v[i].r * v[i].c) + (v[j].r * v[j].c)));
 
-	int ans = 0; // 정답
-	for (int i = 0; i < num - 1; i++) // 입력받은 벡터의 크기 - 1 만큼 반복
-	{
-		int command = sticker(row, col, v[i].r, v[i].c);
-		if (command == 1)
-		{
-
-			for (int j = i + 1; j < num; j++) // 두번째 도형은 i+1 부터 num까지 반복
-			{
-				int n_r = row - max(v[i].r , v[j].r);
-				int n_c = col - max(v[i].c , v[j].c);
-				if (sticker(n_r, col, v[j].r, v[j].c)) ans = max(ans, (v[i].r * v[i].c) + (v[j].r) * (v[j].c));
-				if (sticker(row, n_c, v[j].r, v[j].c)) ans = max(ans, (v[i].r * v[i].c) + (v[j].r) * (v[j].c));
-			}
-		}
-		if (command == 2)
-		{
-
-			for (int j = i + 1; j < num; j++) // 두번째 도형은 i+1 부터 num까지 반복
-			{
-				int n_r = row - max(v[i].c, v[j].c);
-				int n_c = col - max(v[i].c, v[j].c);
-				if (sticker(n_r, col, v[j].r, v[j].c)) ans = max(ans, (v[i].r * v[i].c) + (v[j].r) * (v[j].c));
-				if (sticker(row, n_c, v[j].r, v[j].c)) ans = max(ans, (v[i].r * v[i].c) + (v[j].r) * (v[j].c));
-			}
-		}
-		else continue;
-	}
 	cout << ans;
 }
