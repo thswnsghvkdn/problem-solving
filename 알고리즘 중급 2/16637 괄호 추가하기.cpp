248bc4202467be1dc6e924c0181d2fc285ece45a
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2020-11-03 19:23:41 +0900

    16637 괄호 추가하기 실패

---
+++ 알고리즘 중급 2/16637 괄호 추가하기.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+vector<char> v;
+vector<char> v2;
+int length;
+long long ans;
+
+long long cal(long long n1, long long n2, char op)
+{
+	long long ans;
+	switch (op)
+	{
+	case '+' :
+			ans = n1 + n2;
+			break;
+	case '-':
+		ans = n1 - n2;
+		break;
+	case '*':
+		ans = n1 * n2;
+		break;
+	case '/':
+		ans = n1 / n2;
+		break;
+	}
+	return ans;
+}
+
+void dfs(int index, long long tot)
+{
+	if (index == length)
+	{
+		ans = max(ans, tot);
+		return;
+	}
+	if (index + 1 <= length) {
+		int temp = cal(tot, v[index + 1] - '0', v2[index]);
+		dfs(index + 1, temp);
+	}
+	if (index + 2 <= length)
+	{
+		int temp = cal(v[index + 1] - '0', v[index + 2] - '0', v2[index + 1]);
+		dfs(index + 2, cal(tot, temp , v2[index]));
+	}
+}
+
+int main()
+{
+	int n;
+	string s;
+	cin >> n;
+	cin >> s;
+	int index1 = 0, index2 = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/')
+			v2.push_back(s[i]);
+		else
+			v.push_back(s[i]);
+	}
+	length = n / 2;
+	dfs(0, v[0]);
+
+	cout << ans;
+}
