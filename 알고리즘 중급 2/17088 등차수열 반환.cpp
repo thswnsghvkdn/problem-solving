9622cf638bb19478948d69962a52d8b265278948
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2020-11-11 17:39:31 +0900

    17088 등차수열 반환
    1항과 2항의 공차를 정한후 반복문으로 다음 항들의 공차를 비교해 나간다.

--- 알고리즘 중급 2/17088 등차수열 반환.cpp
+++ 알고리즘 중급 2/17088 등차수열 반환.cpp
@@ -5,29 +5,9 @@
 using namespace std;
 int num; // 수열의 개수
 vector<int> v; // 등차수열 
+vector<int> v2;
 int ans = -1;
-bool check() // 등차수열 검사
-{
-	int d = v[0] - v[1]; // 공차
-	for (int i = 1; i < v.size() - 1; i++)
-		if (d != (v[i] - v[i + 1])) return false; // 공차 검사
-	return true;
-}
-void dfs(int cnt, int index) // 
-{
-	if (index == v.size()) return;
-	if (check()) {
-		if (ans == -1 || ans > cnt)
-			ans = cnt;
-		return;
-	}
-	dfs(cnt, index + 1);
-	v[index] -= 1;
-	dfs(cnt + 1, index + 1);
-	v[index] += 2;
-	dfs(cnt + 1, index + 1);
-	v[index] -= 1;
-}
+
 int main()
 {
 	int temp;
@@ -36,7 +16,55 @@ int main()
 	{
 		cin >> temp;
 		v.push_back(temp);
+		v2.push_back(temp);
 	}
-	dfs(0, 0);
+	int temp_ans = 0;
+	if (num == 1)
+	{
+		cout << 0;
+		return 0;
+	}
+	for (int i = -1; i < 2; i++) {
+		for (int j = -1; j < 2; j++)
+		{
+			temp_ans = 0;
+			int ans_flag = true;
+			v2[0] = v[0] + i;
+			v2[1] = v[1] + j;
+			int diff = v2[0] - v2[1];
+			if (i != 0) temp_ans++;
+			if (j != 0) temp_ans++;
+			for (int k = 2; k < num; k++)
+			{
+				int new_diff = v2[k - 1] - v[k];
+				int flag = false;
+
+				for (int l = -1; l < 2; l++)
+				{
+					if (new_diff + l == diff)
+					{
+						flag = true;
+						v2[k] = v[k] - l;
+						if (l != 0) temp_ans++;
+						break;
+					}
+				}
+				v2[k - 1] = v[k - 1];
+				if (!flag)
+				{
+					ans_flag = false; 
+					break;
+				}
+			}
+			if (ans_flag)
+			{
+				if (ans == -1 || ans > temp_ans)
+					ans = temp_ans;
+			}
+
+			v2[0] = v[0];
+			v2[1] = v[1];
+		}
+	}
 	cout << ans;
 }
