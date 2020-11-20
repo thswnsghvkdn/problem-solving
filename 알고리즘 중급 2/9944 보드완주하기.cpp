745470c8ecfd7fae82c7ae7a12cfccd3880da6fc
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2020-11-20 13:03:09 +0900

    9944 보드완주하기

--- 알고리즘 중급 2/9944 보드완주하기.cpp
+++ 알고리즘 중급 2/9944 보드완주하기.cpp
@@ -4,118 +4,80 @@
 
 using namespace std;
 int n, m;
+int unmarked, marked;
 int dx[] = { -1, 0 , 1, 0 }; // 위 오 아 왼 
 int dy[] = { 0 , 1 , 0 ,-1 };
 char map[31][31];
+bool visit[31][31];
 int ans = -1;
 struct Point
 {
 	int x, y;
 };
 
-void copyMap(char map[31][31], char map2[31][31])
+void dfs(Point p , int dir, int cnt, int tot)
 {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			map2[i][j] = map[i][j];
-}
-void moveMap(Point p , int dir)
-{
-	while (1)
+	if (cnt == unmarked)
 	{
-		int nx = p.x + dx[dir];
-		int ny = p.y + dy[dir];
-		if (nx < 0 || nx >= n || ny < 0 || ny >= m) return;
-		if (map[nx][ny] == '*' || map[nx][ny] == '1') return;
-		map[nx][ny] = '1';
-	}
-}
-bool checkMap()
-{
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			if (map[i][j] == '.') return false;
-	return true;
-}
-bool check(Point p)
-{
-	if (p.x < 0 || p.x >= n || p.y < 0 || p.y >= m)
-		return false;
-	if (map[p.x][p.y] == '*' || map[p.x][p.y] == '1')
-		return false;
-	return true;
-}
-int checkWay(Point p) // 4방향의 상대적위치를 반환
-{
-	int x0 = p.x + dx[0], y0 = p.y + dy[0];
-	int x1 = p.x + dx[1], y1 = p.y + dy[1];
-	int x2 = p.x + dx[2], y2 = p.y + dy[2];
-	int x3 = p.x + dx[3], y3 = p.y + dy[3];
-	bool flag0 = check({ x0 , y0 });
-	bool flag1 = check({ x1 , y1 });
-	bool flag2 = check({ x2 , y2 });
-	bool flag3 = check({ x3 , y3 });
-
-	// 두 방향다 갈 수 있다면 0을 반환
-	if (flag0 && flag2) return 0;
-	if (flag1 && flag3) return 0;
-	if (!flag0 && !flag2 && !flag1 && !flag3) return -1; // 4방향다 갈 수 없다면 -1반환
-
-	if (flag0) return 1;
-	if (flag1) return 2;
-	if (flag2) return 3;
-	if (flag3) return 4;
-
-}
-void dfs(Point p , int dir, int count)
-{
-	char backup[31][31];
-	copyMap(map, backup);
-	int x0 = p.x + dx[0], y0 = p.y + dy[0];
-	int x1 = p.x + dx[1], y1 = p.y + dy[1];
-	int x2 = p.x + dx[2], y2 = p.y + dy[2];
-	int x3 = p.x + dx[3], y3 = p.y + dy[3];
-	bool flag0 = check({ x0 , y0 });
-	bool flag1 = check({ x1 , y1 });
-	bool flag2 = check({ x2 , y2 });
-	bool flag3 = check({ x3 , y3 });
-	int n_dir = checkWay(p);
-	if (n_dir == -1)
-	{
-		checkMap();
-		if (ans != -1 || ans > count)
-			ans = count;
+		if (ans == -1 || ans > tot)
+			ans = tot;
 		return;
 	}
-	if (n_dir == 0)
-		return;
-	if (dir == 4)
+	int nx = p.x + dx[dir];
+	int ny = p.y + dy[dir];
+	if (!visit[nx][ny] && nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == '.') // 더 움직일수 있다면
 	{
-		for (int i = 0; i < 4; i++) {
+		visit[nx][ny] = true;
+		dfs({ nx , ny }, dir, cnt + 1, tot);
+		visit[nx][ny] = false;
+	}
+
+	else {
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == dir) continue;
 			int nx = p.x + dx[i];
 			int ny = p.y + dy[i];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (map[nx][ny] == '*' || map[nx][ny] == '1') continue;
-			dfs({ nx, ny }, i, 1);
+			if (!visit[nx][ny] && nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == '.')
+			{
+				visit[nx][ny] = true;
+				dfs({ nx , ny }, i, cnt + 1, tot + 1);
+				visit[nx][ny] = false;
+			}
 		}
 	}
-	else if (dir == 0 || dir == 2)
-	{
-		if (flag1) dfs({ x1 , y1 }, 1, count + 1);
-		if (flag3) dfs({ x3 , y3 }, 3, count + 1);
-	}
-	else
-	{
-		if (flag0) dfs({ x0 , y0 }, 0, count + 1);
-		if (flag2) dfs({ x2 , y2 }, 2, count + 1);
-	}
 }
-
 
 int main()
 {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			cin >> map[i][j];
+	int index = 1;
+	while (1)
+	{
+		cin >> n >> m;
+		unmarked = 0;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++) 
+			{
+				visit[i][j] = false;
+				cin >> map[i][j];
+				if (map[i][j] == '.')
+					unmarked++;
+			}
+		
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+			{
+				if (map[i][j] == '.') {
+					visit[i][j] = true;
+					for (int k = 0; k < 4; k++)
+						dfs({ i , j }, k, 1, 1);
+					visit[i][j] = false;
+				}
+			}
+	
+		cout << "Case "<< index << ": " << ans <<"\n";
+		index++;
+	}
+
+
 }
