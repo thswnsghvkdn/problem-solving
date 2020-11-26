14d877eebdaa8819d1e25c6ef68c69566ba73ff9
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2020-11-26 15:40:35 +0900

    17089 세친구
     n 제한이 짧아서 3중 반복으로 푸는 것이 가능하다.

--- 알고리즘 중급 2/17089 세친구.cpp
+++ 알고리즘 중급 2/17089 세친구.cpp
@@ -8,24 +8,7 @@ bool friendship[4001][4001];
 int n, m;
 int ans = -1;
 int first;
-void dfs(int cnt, int index, int size)
-{
-	if (cnt == 3 && friendship[first][index])
-	{
-		if (ans == -1 || ans > size)
-			ans = size;
-		return;
-	}
-	for (int i = 0; i < friends[index].size() ; i++)
-	{
-		int temp_num = friends[index][i];
-		int temp_size = size;
-		if (temp_num <= index || friends[temp_num].size() < 2) continue;
-		temp_size += friends[temp_num].size() - 2;
-		dfs(cnt + 1, temp_num, temp_size);
-	}
 
-}
 
 int main()
 {
@@ -38,12 +21,22 @@ int main()
 		friends[t2].push_back(t1);
 		friendship[t1][t2] = friendship[t2][t1] = true;
 	}
-	for (int i = 1; i <= m; i++)
-		sort(friends[i].begin(), friends[i].end());
+
 	for (int i = 1; i <= m; i++) {
-		first = i;
-		if (friends[i].size() < 2) continue;
-		dfs(1, i, friends[i].size() - 2);
+		for (int j = i + 1; j <= m; j++) {
+			if (friendship[i][j] == false) continue;
+			for (int k = j + 1; k <= m; k++)
+			{
+				if (friendship[j][k] == false) continue;
+				if (friendship[i][k])
+				{
+					int temp = friends[i].size() + friends[j].size() + friends[k].size();
+					temp -= 6;
+					if (ans == -1 || ans > temp)
+						ans = temp;
+				}
+			}
+		}
 	}
 	cout << ans;
 }
