653bff2bdac478fa600154929cbb05d7c55dce5b
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2020-12-16 15:50:28 +0900

    17070 파이프 옮기기 1
    2차원 벡터를 이용하여 코드를깔끔하게 변경하기

--- 알고리즘 중급 2/17070 파이프 옮기기 1.cpp
+++ 알고리즘 중급 2/17070 파이프 옮기기 1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int n; // 행렬의 크기
@@ -7,11 +7,11 @@ int mat[17][17]; // 행렬
 int way; // 갈 수 있는 방법 수
 int dx[] = { 0, 1 , 1 }; // 회전 방향 가로 대각선 세로
 int dy[] = { 1, 1, 0 }; // 회전 방향 가로 대각선 세로
-
 struct Point
 {
 	int r ,c , dir;
 }; // 회정정보 구조체 행, 열 , 방향
+vector<int> direction[] = { {0 , 1} , {0 , 1 , 2} , {1 , 2} }; // 갈 수 있는 방향 정보
 
 bool check_dir(Point p, int dir) // 앞으로 갈 방향이 가능한지 검사
 {
@@ -68,13 +68,37 @@ void dfs(Point p)
 			dfs({ p.r + dx[2] ,p.c + dy[2] , 2 }); // 진행한 방향으로 재귀호출
 	}
 }
+vector<int> direction[] = { {0 , 1} , {0 , 1 , 2} , {1 , 2} }; // 갈 수 있는 방향 정보
+void dfs2(Point p)
+{
+
+	if (p.r == n && p.c == n) // 종료조건
+	{
+		way++;
+		return;
+	}
+
+		for (int d : direction[p.dir]) // 해당 인덱스의 갈 수 있는 방향정보만틈 반복
+		{
+			int nx = p.r + dx[d];
+			int ny = p.c + dy[d];
+			if (nx > n || ny > n || mat[nx][ny] == 1) continue;
+			if (d == 1) // 대각선 방향의 경우 빈칸 추가 검사
+			{
+				if (mat[nx - 1][ny] == 0 && mat[nx][ny - 1] == 0)
+					dfs2({ nx , ny , d });
+			}
+			else
+				dfs2({ nx, ny ,d });
+		}
+}
 int main()
 {
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++)
 			cin >> mat[i][j];
-	dfs({ 1 , 2 ,0 });
+	dfs2({ 1 , 2 ,0 });
 	cout << way;
 }
 
