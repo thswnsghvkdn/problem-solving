7f04bb87ae2b6b025dbed5dcd026062218df684b
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2020-12-27 22:11:08 +0900

    십자가 2개 놓기
    2중 반복문으로 탐색하며 두번째 별의 크기를 확인하는 함수를 호출한다.

--- 알고리즘 중급 2/십자가 2개 놓기.cpp
+++ 알고리즘 중급 2/십자가 2개 놓기.cpp
@@ -2,80 +2,66 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int depth[4] = {1, 3 , 5 , 7 }; // 별의 세로 길이
-int star_size[4] = { 1, 5 , 9 ,13 }; // 별의 사이즈
 
+char mp[16][16];
+int n, m;
 
-int n, m;  // 문제에서 주어진 행 , 열 
-int tot = -1;
-void draw_star(char ar[16][16], int r, int c, int size) // 별을 그린다.
+bool check_range(int r, int c, int k) // 0 과 n,m 사이인지 범위를 검사한다.
 {
-	int mid = depth[size] / 2;  // 사이즈의 중앙값
-	for (int i = 0; i < depth[size]; i++) {
-		ar[r + i][c] = '*'; // 세로축 그리기
-		ar[r + mid][c - mid + i] = '*'; // 가로축 그리기
-	}
-}
-
-int check_star( char ar[16][16], int r , int c ) // 맨 위 좌표를 받고 해당 좌표에서 최대로 뻗어나갈 수 있는 별을 반환
-{
-	int i;
-	
-	for (i = 1; i < 4; i++)
+	for (int i = -1; i <= 1; i += 2) // 양방향을 검사한다.
 	{
-		int mid = depth[i] / 2;
-		int d = depth[i];
-		for (int j = 0; j < d; j++ ) // 세로 검사
-		{
-			if (r + j >= n || c - mid + j < 0 || c - mid + j >= m || ar[r + j][c] != '#' || ar[r + mid][c - mid + j] != '#') {
-				draw_star(ar, r , c , i - 1);
-				return i - 1;
-			}
-		}
+		if (r + k * i < 0 || r + k * i >= n || c + k * i < 0 || c + k * i >= m)
+			return false;
 	}
-
-	draw_star(ar, r, c, 3); // 가장 긴 별을 그린다.
-	return 3;
+	return true;
 }
 
-void copy_map(char origin[16][16], char backup[16][16])
+int second_star(int r, int c) // 두 번째 별의 최대 크기를 반환하는 함수
 {
+	int k;
+	int max_size = 1;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
-			backup[i][j] = origin[i][j];
+		{
+			if (mp[i][j] != '#') continue;
+			k = 1;
+			while (check_range(i, j, k) && mp[i + k][j] == '#' && mp[i - k][j] == '#' && mp[i][j + k] == '#' && mp[i][j - k] == '#')
+			{
+				k++;
+			}
+			max_size = max(max_size, --k * 4 + 1);
+		}
+
+	return max_size;
 }
 
 int main()
 {
-	int temp_ans;
-	cin >> n >> m; 
-	char map[16][16];
-	char ori[16][16];
-	char backup[16][16];
+	cin >> n >> m;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
-			cin >> ori[i][j];
+			cin >> mp[i][j];
 
-	copy_map(ori, map);
+	int first_size, second_size, k, tot_size = 0;
 	
 	for(int i = 0 ; i < n ; i++)
 		for (int j = 0; j < m; j++)
 		{
-			if (ori[i][j] == '#') {
-				temp_ans = star_size[check_star(map, i, j)];
-				copy_map(map, backup);
-				for (int k = i; k < n; k++)
-					for (int l = 0; l < m; l++)
-					{
-						if (k == i && l <= j) continue;
-						if (map[k][l] == '#') {
-							tot = max(tot, temp_ans * star_size[check_star(backup, k, l)]);
-							copy_map(map, backup);
-						}
-					}
-				copy_map(ori, map);
+			if (mp[i][j] != '#') continue;
+			k = 0;
+			while (check_range(i, j, k) && mp[i + k][j] == '#' && mp[i - k][j] == '#' && mp[i][j + k] == '#' && mp[i][j - k] == '#')
+			{
+				mp[i + k][j] = mp[i - k][j] = mp[i][j + k] = mp[i][j - k] = '.';
+				first_size = k * 4 + 1;
+				second_size = second_star(i, j);
+				tot_size = max(tot_size, first_size * second_size);
+				k++;
 			}
+			
+			for(int l = 0; l < k ; l++)
+				mp[i + l][j] = mp[i - l][j] = mp[i][j + l] = mp[i][j - l] = '#';
 		}
 
-	cout << tot;
+	cout << tot_size;
 }
+
