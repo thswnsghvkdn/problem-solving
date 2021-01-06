a84c4bd7e8c204509b752efbbee7e8f51ef0ba64
LAPTOP-RLSB5C3K\82107 <ths3630802@naver.com>
2021-01-06 12:06:04 +0900

    16986 인싸들의 가위바위보
    문제이해가 어려워 한참걸렸다.

--- 알고리즘 중급 2/16986 인싸들의 가위바위보.cpp
+++ 알고리즘 중급 2/16986 인싸들의 가위바위보.cpp
@@ -1,121 +1,57 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
-struct Player {
-	int hand[20];
-	int win; // 이긴횟수
-	int round; // 플레이할 라운드
-};
-
-// 두 명의 플레이어가 낼 손
-int hand1[20];
-int hand2[20];
-
 int n, k;
-int rule[10][10]; // 가위바위보 족보
+int winning[11][11];
+int hand[3][21];
 
-int who_win(int p1, int p2) // 앞 사람과 뒤사람중 이긴 사람반환
+bool isOneWinner()
 {
-	if (rule[p1][p2] == 2) // p1이 이긴경우
+	int p1 = 0, p2 = 1, p3 = 2;
+	int idx[3] = { 1 ,1 ,1 };
+	int win[3] = { 0 ,0 ,0 };
+
+	while (1)
 	{
-		return 1;
+		if (p1 > p2) swap(p1, p2);
+		if (p1 == 0, idx[0] > n) break;
+		if (idx[p1] >= 21 || idx[p2] >= 21) break;
+
+		if (winning[hand[p1][idx[p1]++]][hand[p2][idx[p2]++]] == 2)
+		{
+			if (++win[p1] == k) break;
+			swap(p2, p3);
+		}
+		else {
+			if (++win[p2] == k) break;
+			swap(p1, p3);
+		}
 	}
-	else
-	{
-		return 2;
-	}
+
+	return win[0] >= k;
 }
 
-int dfs(Player p[3], int p1, int p2) // 이긴 횟수와 각자 손을 낼 차례. 플레이할 두명
-{
-	for(int i = 0 ; i < 3 ; i++)
-		if (p[i].win == k)
-		{
-			if (i == 0) {
-				cout << 1;
-				return 1;
-			}// 지우가 이긴경우
-			else return 0;
-		}
-
-	if (p1 == 0)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			if (i == p[0].hand[p[0].round]) continue;
-			p[0].hand[++p[0].round] = i;
-			if (who_win(i, p[p2].hand[p[p2].round++]) == 1) {
-				p[0].win++;
-				if( dfs(p, 0, 3 - p2) == 1 ) return 1;
-				p[0].win--;
-			}
-			else
-			{
-				p[p2].win++;
-				if(dfs(p, p2, 3 - p2) == 1 ) return 1;
-				p[p2].win--;
-			}
-			p[0].round--;
-			p[p2].round--;
-		}
-	}
-	else if (p2 == 0)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			if (i == p[0].hand[p[0].round]) continue;
-			p[0].hand[++p[0].round] = i;
-
-			if (who_win(p[p1].hand[p[p1].round++], i) == 1) 
-			{
-				p[p1].win++;
-				if( dfs(p, p1, 3 - p1) == 1 ) return 1;
-				p[p1].win--;
-			}
-			else
-			{
-				p[0].win++;
-				if( dfs(p, 0, 3 - p1) ) return 1;
-				p[0].win--;
-			}
-			p[0].round--;
-			p[p1].round--;
-		}
-	}
-	else
-	{
-		if (who_win(p[p1].hand[p[p1].round++], p[p2].hand[p[p2].round++]) == 1)
-			return dfs(p, p1, 0);
-		else
-			return dfs(p, p2, 0);
-	}
-
-
-}
 
 int main()
 {
 	cin >> n >> k;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			cin >> rule[i][j];
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> winning[i][j];
+	for (int i = 1; i < 21; i++)
+		cin >> hand[1][i];
+	for (int i = 1; i < 21; i++)
+		cin >> hand[2][i];
+	for (int i = 1; i <= n; i++)
+		hand[0][i] = i;
 
-	Player p[3];
-
-	for (int i = 0; i < 3; i++)
-		p[i].round = p[i].win = 0;
-
-	for (int i = 0; i < 20; i++)
-		cin >> p[1].hand[i];
-	for (int i = 0; i < 20; i++)
-		cin >> p[2].hand[i];
-
-	for (int i = 0; i < n; i++)
+	do
 	{
-		p[0].hand[0] = i;
-		if (dfs(p, 0 ,1) == 1 ) return 0;
-		for (int j = 0; j < 3; j++)
-			p[j].round = p[j].win = 0;
-	}
+		if (isOneWinner()) {
+			cout << 1;
+			return 0;
+		}
+	} while (next_permutation(hand[0] + 1, hand[0] + n + 1));
+	cout << 0;
 }
