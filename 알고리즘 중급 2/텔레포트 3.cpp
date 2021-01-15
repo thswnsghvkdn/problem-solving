0d01c730c6f7e9f609b7afcfc6a2415aae5d6465
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2021-01-15 20:39:39 +0900

    12908 텔레포트 3
    플로이드 와샬 알고리즘

--- 알고리즘 중급 2/텔레포트 3.cpp
+++ 알고리즘 중급 2/텔레포트 3.cpp
@@ -4,30 +4,29 @@
 using namespace std;
 
 
-int ar[8][8];
-vector<pair<int, int>> v(8);
+long long ar[8][8];
+vector<pair<long, long>> v(8);
 
-int dist(int x1,int x2,int y1,int y2)
+int dist(long long x1,long long x2,long long y1,long long y2)
 {
 	return abs((x1)-(x2)) + abs((y1)-(y2));
 }
 int main()
 {
-	int tx, ty;
+	long long tx, ty;
 	for (int i = 0; i < 8; i++)
 	{
 		cin >> tx >> ty;
 		v[i].first = tx;
 		v[i].second = ty;
 	}
-	ar[1][0] = ar[0][1] = dist(v[0].first, v[1].first, v[0].second, v[1].second);
 	
 	for(int i = 0 ; i < 8 ; i++)
 		for (int j = 0; j < 8; j++)
 			ar[i][j] = dist(v[i].first, v[j].first, v[i].second, v[j].second);
 		
 	for (int i = 2; i < 8; i += 2)
-		ar[i][i + 1] = ar[i + 1][i] = min( 10 , ar[i][i+1] );
+		ar[i][i + 1] = ar[i + 1][i] = min( (long long)10 , ar[i][i+1] );
 
 	for(int k = 0 ; k < 8 ; k++)
 		for(int i = 0 ; i < 8 ; i++)
