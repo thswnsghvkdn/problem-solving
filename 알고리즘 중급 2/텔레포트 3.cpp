9d7a3421cd9b0ad631535b5d692f495572d8f6a6
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2021-01-18 17:34:33 +0900

    12908 텔레포트 3 실패
    순열을 이용하여 풀기 틀린 곳 찾기 어려움

--- 알고리즘 중급 2/텔레포트 3.cpp
+++ 알고리즘 중급 2/텔레포트 3.cpp
@@ -1,41 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
 #include <algorithm>
 using namespace std;
 
-
-long long ar[8][8];
-vector<pair<long, long>> v(8);
-
-int dist(long long x1,long long x2,long long y1,long long y2)
+long long dist(long long x1, long long x2, long long y1, long long y2)
 {
 	return abs((x1)-(x2)) + abs((y1)-(y2));
 }
+struct Point {
+	long long start, end;
+};
+vector<pair<Point, Point>> v(7);
+vector<int> pri(7);
+
 int main()
 {
-	long long tx, ty;
-	for (int i = 0; i < 8; i++)
-	{
-		cin >> tx >> ty;
-		v[i].first = tx;
-		v[i].second = ty;
+	long long xs, ys;
+	long long xe, ye;
+	cin >> xs >> ys >> xe >> ye;
+	long long ans = INT_MAX;
+
+	for (int i = 0; i < 6; i += 2) {
+		cin >> v[i].first.start >> v[i].first.end >> v[i].second.start >> v[i].second.end;
+		v[i + 1].first = v[i].second;
+		v[i + 1].second = v[i].first;
 	}
-	
-	for(int i = 0 ; i < 8 ; i++)
-		for (int j = 0; j < 8; j++)
-			ar[i][j] = dist(v[i].first, v[j].first, v[i].second, v[j].second);
-		
-	for (int i = 2; i < 8; i += 2)
-		ar[i][i + 1] = ar[i + 1][i] = min( (long long)10 , ar[i][i+1] );
+	v[6].first.start = xe;
+	v[6].first.end = ye;
+	v[6].second.start = xe;
+	v[6].second.end = ye;
 
-	for(int k = 0 ; k < 8 ; k++)
-		for(int i = 0 ; i < 8 ; i++)
-			for (int j = 0; j < 8; j++)
-			{
-				if (i == j) continue;
-				ar[i][j] = min(ar[i][j], ar[i][k] + ar[k][j]);
-			}
+	for (int i = 0; i < 7; i++)
+		pri[i] = i;
 
-	cout << ar[0][1];
+	do
+	{
+		int nx = xs, ny = ys;
+		int t = 0;
+		for (int i = 0; i < 7; i++)
+		{
+			t += dist(nx, v[pri[i]].first.start, ny, v[pri[i]].first.end);
+			if (v[pri[i]].second.start == xe && v[pri[i]].second.end == ye) break;
+			t += 10;
+			nx = v[pri[i]].second.start;
+			ny = v[pri[i]].second.end;
+		}
+		if (t < ans)
+			ans = t;
+	} while (next_permutation(pri.begin(), pri.end()));
 
+	cout << ans;
 }
