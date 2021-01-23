0e39d9dfd58e9749c8d810c8aeccf2b02e6cf84a
thswnsghvkdn <ths3630802@naver.com>
2021-01-23 13:58:44 +0900

    16971 배열 B의 값
    각 행 , 열만 조사하는 것이 아닌 조건의 맞는 sum값을 비교

--- 알고리즘 중급 2/16971 배열 B의 값.cpp
+++ 알고리즘 중급 2/16971 배열 B의 값.cpp
@@ -9,7 +9,7 @@ int B[1001][1001];
 pair<int, int> maxRow = { INT_MIN , 0 }, maxCol = { INT_MIN , 0 };
 pair<int, int> minRow = { INT_MAX , 0 }, minCol = { INT_MAX, 0 };
 int row_first, row_last, col_first, col_last;
-void swap_line(char c , int index1 , int index2)
+void swap_line(char c, int index1, int index2)
 {
 	int t;
 	if (c == 'c')
@@ -60,8 +60,8 @@ int main()
 			row_first = t;
 		if (i == n - 1)
 			row_last = t;
-		if (minRow.first > t)
-			minRow = { t , i };
+		if (i != 0 && i != n - 1 && minRow.first > ( t * 4 - 2 * (A[i][0] + A[i][m-1]) ))
+			minRow = { t * 4 - 2 * (A[i][0] + A[i][m - 1]) , i };
 	}
 	for (int j = 0; j < m; j++) {
 		int t = 0;
@@ -71,8 +71,8 @@ int main()
 			col_first = t;
 		if (j == m - 1)
 			col_last = t;
-		if (minCol.first > t)
-			minCol = { t , j };
+		if (j != 0 && j != m - 1 && minCol.first > ( t * 4 - 2 * (A[0][j] + A[n-1][j]) ))
+			minCol = { t * 4 - 2 * (A[0][j] + A[n - 1][j]) , j };
 	}
 
 	int ans[5];
@@ -91,11 +91,11 @@ int main()
 
 
 	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++) 
+		for (int j = 0; j < m; j++)
 			A[i][j] = B[i][j];
 	swap_line('r', minRow.second, 0);
 	ans[3] = ret_tot(A);
-	
+
 
 
 	for (int i = 0; i < n; i++)
@@ -107,8 +107,8 @@ int main()
 
 
 	int n_ans = INT_MIN;
-	for(int i = 0 ; i < 5 ;i++)
-		if(n_ans < ans[i])
+	for (int i = 0; i < 5; i++)
+		if (n_ans < ans[i])
 			n_ans = ans[i];
 	cout << n_ans;
 }
